2c75eb9b7fe6b122c5ffa46e4b6f49223535e990
alexander Patapoff <awpatapoff@gmail.com>
2015-01-12 19:57:43 -0800

    Character_Stats_Change_01
    created variablespertaining to the character.
    set values to the variables

---
+++ util/Character.java
@@ -0,0 +1,21 @@
+package util;
+
+public class Character {
+	static private int health;
+	static private double armor;
+	static private int attackDmg;
+	static private double speed;
+	static private int weapon_Dmg;
+	static private double speed_Multiplier;
+	public static void character_Stats()
+		{
+			health = 100;
+			armor = 0.10;
+			speed = 10.0;
+			speed_Multiplier = 0.05;
+			attackDmg = 5;
+			weapon_Dmg = 0;
+			
+		}
+
+}
