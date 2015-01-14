6d580e5f747f8d10034e0f73951de0d97e89937c
Troi-Ryan Stoeffler <troiryan2@gmail.com>
2015-01-13 20:58:25 -0800

    set up example stage showing off buttons

--- StageMenu.java
+++ StageMenu.java
@@ -3,11 +3,12 @@ import ui.Stage;
 public class StageMenu extends Stage {
     @Override
     public void choiceDone(String a) {
-        if(a.equals("hi")) setChoices("hi", "test", "Done", "adf", "dsfsdfs");
+        if(a.equals("Click To Expand")) setChoices("Click To Minimize", "Disable Me", "Disable Me", "Disable Me", "Disable Me", "Disable Me");
+        else if(a.equals("Click To Minimize")) init();
     }
     
     @Override
     public void init() {
-        setChoices("hi");
+        setChoices("Click To Expand", "Disable Me");
     }
 }
