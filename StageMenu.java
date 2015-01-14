18e2c201fa497add081f584843a60af2c7c4fc48
Troi-Ryan Stoeffler <troiryan2@gmail.com>
2015-01-13 23:15:22 -0800

    added a simple scene example : P

--- StageMenu.java
+++ StageMenu.java
@@ -4,7 +4,7 @@ public class StageMenu extends Stage {
     @Override
     public void choiceDone(String a) {
         setDialog("Minimized");
-        setImages("");
+        setImages("troi", "troi");
         if(a.equals("Click To Expand")) setChoices("Click To Minimize", "Disable Me", "Disable Me", "Disable Me", "Disable Me", "Disable Me");
         else if(a.equals("Click To Minimize")) init();
     }
