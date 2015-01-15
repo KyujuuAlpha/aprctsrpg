001bd894e5f9eb3d38b5fb6a5e698e8404288cb2
alexander Patapoff <awpatapoff@gmail.com>
2015-01-14 18:20:30 -0800

    Item_MasterKey
    added some code into the Item_MasterKey
    ****IS NOT FINAL****

--- Item_MasterKey.java
+++ Item_MasterKey.java
@@ -1,4 +1,23 @@
 
 public class Item_MasterKey {
+	static double[] stats_Value = new double[2];
+	static String[] stats_Name = {"damage", "drop_Num","poisonDotDamage"};
+	public static void set_Stats(double damage, double drop_Num,double poisonDotDmg)
+		{
+			stats_Value[0] = damage;
+			stats_Value[1] = drop_Num;
+			stats_Value[2] = poisonDotDmg;
+		}
+	public static double stat_Return(String stat)
+		{
+		for (int i = 0; i <= stats_Name.length; i++)
+			{
+				if (stats_Name[i].equals(stat))
+					{
+						return stats_Value[i];
+					}
+			}
+		return 0;
+		}
 
 }
