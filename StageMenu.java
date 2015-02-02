0615f91bedf4234731ca953b53c0d17c130f728f
Ryan Chen <ryanjchen@yahoo.com>
2015-02-02 11:42:56 -0800

    More STory
    more story

--- StageMenu.java
+++ StageMenu.java
@@ -6,11 +6,12 @@ public class StageMenu extends Stage {
     private Choice a;
     private Choice b;
     @Override
-    public void choiceDone(String a) {
-        if(a.equals("START")){
-            
-            stageOne();
-        }else if(a.equals("QUIT")){
+    public void choiceDone(String choiceName) {
+        if(choiceName.equals("START")){
+            a.setLabel("A", true);
+            b.setLabel("B", false);
+            stageOnePointOne();
+        }else if(choiceName.equals("QUIT")){
         }
     }
     
@@ -28,8 +29,7 @@ public class StageMenu extends Stage {
         this.addElement(mainDialog, a, b);
     }
     
-    public void stageOne(){
-        mainDialog.setText("It's the year 151515...", "Press A to Continue");
-        this.addElement(mainDialog, new Choice("A"));
+    public void stageOnePointOne(){
+        mainDialog.setText("It's the year 151515, after the Last World War, with", "President Obama L finally dead,", "anarchy reigns as Ebola Z runs throughout the world. As Brad", "Pitt and Angelina Jolie rebuild ", "their giant mansion on the newly formed Hawaiian island of N'vu L'nodo,", "Robert Downey Jr.finally accepts his responsibility as Iron Man", "and forms SHIELD along with Samuel Jackson. ", "You are stuck in Seattle, the pot capital of the world, as you try", " to save the human race from ", "the deadly disease. Brad Pitt and Angelina Jolie are rumored to have protection and their own ", "private army, but SHIELD offers the Avengers and the Guardians of the Galaxy. With SHIELD there", "is a hope of being transported to another dimension. How will you proceed?", "Press A to Continue");
     }
 }
