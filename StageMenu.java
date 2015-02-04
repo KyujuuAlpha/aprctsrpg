786ffca30508113683c49685293aec3e97560000
Ryan Chen <ryanjchen@yahoo.com>
2015-02-04 11:43:17 -0800

    renamed stageOnePointOne to stagePre

--- StageMenu.java
+++ StageMenu.java
@@ -10,7 +10,7 @@ public class StageMenu extends Stage {
         if(choiceName.equals("START")){
             a.setLabel("A", true);
             b.setLabel("B", false);
-            stageOnePointOne();
+            stagePre();
         }else if(choiceName.equals("QUIT")){
         }
     }
@@ -29,7 +29,12 @@ public class StageMenu extends Stage {
         this.addElement(mainDialog, a, b);
     }
     
-    public void stageOnePointOne(){
+    public void stagePre(){
         mainDialog.setText("It's the year 151515, after the Last World War, with", "President Obama L finally dead,", "anarchy reigns as Ebola Z runs throughout the world. As Brad", "Pitt and Angelina Jolie rebuild ", "their giant mansion on the newly formed Hawaiian island of N'vu L'nodo,", "Robert Downey Jr.finally accepts his responsibility as Iron Man", "and forms SHIELD along with Samuel Jackson. ", "You are stuck in Seattle, the pot capital of the world, as you try", " to save the human race from ", "the deadly disease. Brad Pitt and Angelina Jolie are rumored to have protection and their own ", "private army, but SHIELD offers the Avengers and the Guardians of the Galaxy. With SHIELD there", "is a hope of being transported to another dimension. How will you proceed?", "Press A to Continue");
+    }
+    
+    public void stageOne(){
+        mainDialog.setText("What CLASS will you choose?", "Tank - High Health", "Normal - Average", "Assassin - High Speed/Attack");
+        
     }
 }
