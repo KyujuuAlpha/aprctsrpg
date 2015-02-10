791048ff801bdc1e6fd32a0a5d3fc2c57964455b
Troi-Ryan Stoeffler <troiryan2@gmail.com>
2015-02-09 16:08:27 -0800

    comment code for stage and sync al elements at once

--- ui/Stage.java
+++ ui/Stage.java
@@ -5,10 +5,13 @@ import ui.elem.*;
 import java.util.ArrayList;
 
 public abstract class Stage {
+    //declare and initialize a new arraylist with the type of element
     private ArrayList<Element> elementList = new ArrayList<Element>();
     
+    //declare a Display instance variable
     private Display gameVar;
     
+    //declare a long variable for task scheduling
     private long ticks = 0;
     
     /**
@@ -31,14 +34,14 @@ public abstract class Stage {
      * 1 tick is equivalent to 1 twentieth of a second
      */
     public void scheduleTask(long intVar) { 
-        ticks += intVar;
-        if(ticks == 0 && intVar == 0) this.taskPerformed();
+        ticks += intVar; //add the specified number of ticks to the countdown 
+        if(ticks == 0 && intVar == 0) this.taskPerformed(); //if for some reason they are both 0, just call the action method
     }
     
-    public void decreaseTicks() {
-        if(ticks > 0) {
-            ticks--;
-            if(ticks == 0) this.taskPerformed();
+    public void decreaseTicks() { //called whenever the timer ticks
+        if(ticks > 0) { //if the countdown is greater than 0, then
+            ticks--; //decrease it
+            if(ticks == 0) this.taskPerformed(); //if it just turned 0, run the action method
         }
     }
     
@@ -46,56 +49,55 @@ public abstract class Stage {
      * Add a new element to this stage;
      */
     public void addElements(Element... elementVar) {
-        for(Element element : elementVar) {
-            if(elementList.indexOf(elementVar) < 0) {
-                elementList.add(element);
-                element.setGameInstance(gameVar);
-                element.createElement();
+        for(Element element : elementVar) { //foreach element in the array
+            if(elementList.indexOf(elementVar) < 0) { //if that element does not already exist
+                elementList.add(element); //then add it to the element list
+                element.setGameInstance(gameVar); //sest it's display instance variable
+                element.createElement(); //create it to avoid null pointer exceptions
             }
         }
-        syncElements();
     }
     
     /**
      * Remove the given elements from this stage
      */
     public void removeElements(Element... elementVar) {
-        for(Element element : elementVar) {
-            elementList.remove(element);
-            element.removeElement();
+        for(Element element : elementVar) { 
+            elementList.remove(element); //remove that element if it exists in the list
+            element.removeElement(); //invoke the element's removeElement method
         }
-        syncElements();
     }
     
     public void syncElements() {
         for(int i = 0; i < elementList.size(); i++) {
-            Element element = elementList.get(i);
-            element.sync();
+            Element element = elementList.get(i); //get the current element in the element list
+            element.sync(); //invoke it's sync method
         }
     }
 
     public void removeElements() {
         for(int i = 0; i < elementList.size(); i++) {
             Element element = elementList.get(i);
-            element.removeElement();
+            element.removeElement(); //invoke it's remove method!
         }
+        elementList.clear(); //remove all the elements from the elementList
     }
     
     /**
      * Go to the next stage in the list
      */
     public void nextStage() {
-        gameVar.nextStage();
+        gameVar.nextStage(); //move onto the next stage by invoking the nextstage method from the displayinstance
     }
     
     /**
      * Go to the previous stage in the list
      */
     public void prevStage() {
-        gameVar.prevStage();
+        gameVar.prevStage(); //invoke the prevstage method from the display instance
     }
     
     public void setGameInstance(Display displayVar) {
-        gameVar = displayVar;
+        gameVar = displayVar; //set the display instance to the specified parameter
     }
 }
