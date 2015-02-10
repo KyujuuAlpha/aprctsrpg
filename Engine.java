084447522c4dc582f07d2343b58596df3b5f397e
LevoSpacewars <awpatapoff@gmail.com>
2015-02-10 11:51:26 -0800

    fixed merge -troi

--- Engine.java
+++ Engine.java
@@ -4,12 +4,8 @@ import util.Utilities;
 
 public class Engine {
     public static void main(String[] args) {
-<<<<<<< HEAD
     	Utilities.init();
-        Display displayVar = new Display("Window");
-=======
         Display displayVar = new Display("aprctsrpg");
->>>>>>> origin/master
         displayVar.addStage(new StagePrologue());
         displayVar.addStage(new StageOne());
         displayVar.addStage(new StageTwo());
