9ece8da94beb6bf21df78da7e78dd71723649dd7
alexander Patapoff <awpatapoff@gmail.com>
2015-02-24 23:16:40 -0800

    Abilities added

--- util/Abilities.java
+++ util/Abilities.java
@@ -7,4 +7,35 @@ public class Abilities {
 		this.player = player;
 		
 	}
+	public double damageAbility(){
+		return 0;
+	}
+	
+	public String damageAbilityDesc(){
+		String x = "NULL";
+		return x;
+	}
+	public double defenceAbility(){
+		return 0;
+	}
+	public String defenceAbilityDec(){
+		String x = "NULL";
+		return x;
+	} 
+	public double utilAbility(){
+		return 0;
+		
+	}
+	public String utilAbilityDesc(){
+		String x = "NULL";
+		return x;
+	}
+	public double utliAbility(){
+		return 0;
+	}
+	public String ultiAbilityDesc(){
+		String x = "NULL";
+		return x;
+				
+	}
 }
--- util/tankAbilitites.java
+++ util/tankAbilitites.java
@@ -1,15 +1,38 @@
 package util;
 
-public class tankAbilitites {
+public class tankAbilitites extends Abilities{
 	public tankAbilitites(EntityPlayer player){
-		super();
+		super(player);
 	}
 	public double damageAbility(){
-		return player.getDamage() * 2
+		return player.getDamage() * 2;
 	}
 	
 	public String damageAbilityDesc(){
-		String x = "You thrust your sword into the ground causing the earth to split before you, cuasing you to deal x2 added damage to the enemy";
+		String x = "You thrust your weapon into the ground causing the earth to split before you, cuasing you to deal x2 added damage to the enemy";
 		return x;
+	}
+	public double defenceAbility(){
+		return player.getArmor() + (player.getArmor() / 2);
+	}
+	public String defenceAbilityDec(){
+		String x = "you use feel energy surrounding your body as your skin hardens to iron cuasing your armor to increase by 1/2";
+		return x;
+	} 
+	public double utilAbility(){
+		return player.getSpeed() + 0.2;
+		
+	}
+	public String utilAbilityDesc(){
+		String x = "you become insane and increase agility for a short time";
+		return x;
+	}
+	public double utliAbility(){
+		return 1.00;
+	}
+	public String ultiAbilityDesc(){
+		String x = "You are granted a blessing from the gods and are invincible for a moment";
+		return x;
+				
 	}
 }
