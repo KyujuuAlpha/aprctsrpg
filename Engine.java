ba90ac46ee4c3e89abf3bc92381ae9fbdf18a794
Troi-Ryan Stoeffler <troiryan2@gmail.com>
2015-02-24 22:13:14 -0800

    new name

--- Engine.java
+++ Engine.java
@@ -4,7 +4,7 @@ import util.Utilities;
 public class Engine {
     public static void main(String[] args) {
     	Utilities.init();
-        Display displayVar = new Display("aprctsrpg");
+        Display displayVar = new Display("AP-RC-TS RPG");
         displayVar.addStage(new StageBattle()); // 0
         displayVar.addStage(new StagePrologue()); //1
         displayVar.addStage(new StageTeam()); //2
