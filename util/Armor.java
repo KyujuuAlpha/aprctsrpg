cf230bd622763fce8c19db903bb6b6da084478f1
LevoSpacewars <awpatapoff@gmail.com>
2015-02-26 10:19:18 -0800

    comment code

--- util/Armor.java
+++ util/Armor.java
@@ -1,13 +1,13 @@
 package util;
 
-public class Armor extends Item{
+public class Armor extends Item{//inherits from Item
 	private double armor;
 	public Armor(){
 		super("defualtArmor");
 	}
 	public Armor(double armor, String name){
 		super(name);
-		this.armor = armor;
+		this.armor = armor;//assigns the value from this method to the variable form this class
 	}
 	public double getArmor(){
 		return armor;
